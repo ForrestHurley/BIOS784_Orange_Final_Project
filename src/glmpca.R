7fc03e00e653b7cc406ab1de65c6f494756fe958
Xinxin Chen <xinxinchen476@gmail.com>
2022-12-01 19:38:58 -0500

    Update glmpca.R

--- src/glmpca.R
+++ src/glmpca.R
@@ -1,13 +1,15 @@
 library(glmpca)
 library(ggplot2)
 library(NatParksPalettes)
+library(gridExtra)
 
 source("src/get_data.R")
-rm(d, info, zeros)
 count <- as.matrix(assay(se))
 count <- as.matrix(count)
+metadata <- as.data.frame(colData(se))
 colnames(count) <- metadata$UniqueCell_ID
 rownames(count) <- rowData(se)$geneID
+
 metadata$Tissue <- sapply(metadata$sampleType, function(x){
   if (x %in% c("NTC", "NTH", "NTR")) {
     return("Adjacent Normal Lung Tissues")
@@ -18,58 +20,45 @@ metadata$Tissue <- sapply(metadata$sampleType, function(x){
   }
 })
 
-metadata$Cell_type <- sapply(metadata$majorCluster, function(x){
-  if (substr(x, 1, 3) == "CD4") {
-    return("CD4")
-  } else if (substr(x, 1, 3) == "CD8") {
-    return("CD8")
-  } else{
-    return("Other")
-  }
-})
-  
 # GLM-PCA
 
 # Negative Binomial:
-#res <- glmpca(count, 2, fam = "nb")
-#saveRDS(res, "data/processed_data/glmpca_nb.rds")
-
 res_pc30 <- glmpca(count, 30, fam = "nb")
 saveRDS(res, "data/processed_data/glmpca_nb_30pcs.rds")
 
 factors <- res_pc30$factors
 metadata["PC_1"] = factors[,1]
 metadata["PC_2"] = factors[,2]
-# metadata["PC_3"] = factors[,3]
-# metadata["PC_3_cat"] = cut(factors[,3], breaks=c(-Inf, -10, -5, 0, 5, 10, Inf))
 
-ggplot(aes(x=PC_1,y=PC_2,color=sampleType),data=metadata) + 
+# color by sample type and tissue
+p1 = ggplot(aes(x=PC_1,y=PC_2,color=sampleType),data=metadata) + 
   geom_point(alpha=0.7) +
-  #facet_wrap(~PC_3_cat) + 
+  labs(subtitle = "Color by Sample Type") + 
   scale_color_brewer(palette="Paired") + 
   theme_bw()
-
-ggplot(aes(x=PC_1,y=PC_2,color=Tissue),data=metadata) + 
+p2 = ggplot(aes(x=PC_1,y=PC_2,color=Tissue),data=metadata) + 
   geom_point(alpha=0.7) +
-  #facet_wrap(~PC_3_cat) + 
+  labs(subtitle = "Color by Tissue") +
   scale_color_brewer(palette="Paired") + 
   theme_bw() +
   theme(legend.position = "bottom")
 
+grid.arrange(p1, p2, ncol = 2)
+
+# color by patient
 ggplot(aes(x=PC_1,y=PC_2,color=Patient),data=metadata) + 
   geom_point(alpha=0.7) +
+  labs(subtitle = "Color by Patient") +
   theme_bw() 
 
-#ggplot(aes(x=PC_1,y=PC_2,color=Cell_type),data=metadata) + 
-#  geom_point(alpha=0.7) +
-  #facet_wrap(~PC_3_cat) + 
-#  scale_color_brewer(palette="Paired", name = "Cell Type") + 
-#  theme_bw()
+# color by major cluster
+ggplot(aes(x=PC_1,y=PC_2,color=majorCluster),data=metadata) + 
+  geom_point(alpha=0.7) +
+  labs(subtitle = "Color by Major Cluster") +
+  theme_bw()
 
 
 # Poisson:
-#res2 <- glmpca(count, 2, fam = "poi")
-#saveRDS(res2, "data/processed_data/glmpca_poi.rds")
 res2_pc30 <- glmpca(count, 30, fam = "poi")
 saveRDS(res2_pc30, "data/processed_data/glmpca_poi_30pcs.rds")
 
@@ -78,23 +67,30 @@ metadata2 = subset(metadata, select = c(UniqueCell_ID, Patient, majorCluster,
 factors2 <- res2_pc30$factors
 metadata2["PC_1"] = factors2[,1]
 metadata2["PC_2"] = factors2[,2]
-# metadata2["PC_3"] = factors2[,3]
-# metadata2["PC_3_cat"] = cut(factors2[,3], breaks=c(-Inf, -10, -5, 0, 5, 10, Inf))
 
-ggplot(aes(x=PC_1,y=PC_2,color=sampleType),data=metadata2) + 
+# color by sample type and tissue
+p1 = ggplot(aes(x=PC_1,y=PC_2,color=sampleType),data=metadata2) + 
   geom_point(alpha=0.7) +
-  #facet_wrap(~PC_3_cat) + 
+  labs(subtitle = "Color by Sample Type") + 
   scale_color_brewer(palette="Paired") + 
   theme_bw()
-
-ggplot(aes(x=PC_1,y=PC_2,color=Tissue),data=metadata2) + 
+p2 = ggplot(aes(x=PC_1,y=PC_2,color=Tissue),data=metadata2) + 
   geom_point(alpha=0.7) +
-  #facet_wrap(~PC_3_cat) + 
+  labs(subtitle = "Color by Tissue") +
   scale_color_brewer(palette="Paired") + 
   theme_bw() +
   theme(legend.position = "bottom")
 
+grid.arrange(p1, p2, ncol = 2)
+
+# color by patient
 ggplot(aes(x=PC_1,y=PC_2,color=Patient),data=metadata2) + 
   geom_point(alpha=0.7) +
+  labs(subtitle = "Color by Patient") +
   theme_bw() 
 
+# color by major cluster
+ggplot(aes(x=PC_1,y=PC_2,color=majorCluster),data=metadata2) + 
+  geom_point(alpha=0.7) +
+  labs(subtitle = "Color by Major Cluster") +
+  theme_bw()
